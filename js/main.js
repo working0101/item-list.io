e70927d57342cbc027154da1d85a6850b7de073a
working0101 <110939305+working0101@users.noreply.github.com>
2023-01-14 21:26:51 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
 'use strict'
+// 
 {
+  const titlePics = document.querySelectorAll('.titlePic');
+  const mainImge = document.getElementById('main-imge');
+  const next = document.getElementById('next')
+  const prev = document.getElementById('prev')
+  
+  let activeIndex = 0;
+
+
+  next.addEventListener('click', () => {
+    titlePics.forEach(element => {
+      titlePics[activeIndex].classList.remove('active')
+    }); 
+    activeIndex++;
+    if (activeIndex === titlePics.length ) {
+      activeIndex = 0;
+      }
+
+      mainImge.src = titlePics[activeIndex].src;
+      titlePics[activeIndex].classList.add('active');
+  });
+  
+  prev.addEventListener('click', () => {
+    titlePics.forEach(element => {
+      titlePics[activeIndex].classList.remove('active')
+    });
+    if (activeIndex === 0 ) {
+      activeIndex = titlePics.length -1 ;
+    } else {
+      activeIndex--
+    }
+      mainImge.src = titlePics[activeIndex].src;
+      titlePics[activeIndex].classList.add('active');
+  });
+  
+  
+    
+
+  // titlePics[1].addEventListener('click', () => {
+  //   mainImge.src = titlePics[1].src;
+  //   titlePics[1].classList.add('active');
+  //   titlePics[0].classList.remove('active')
+  // });
+
+
+
+// accordion
+
   const dts = document.querySelectorAll('dt');
 
   dts.forEach(dt => {
@@ -7,7 +55,7 @@
       dt.parentNode.classList.toggle('appear')
 
       dts.forEach(el => {
-        if(dt !== el) {
+        if (dt !== el) {
           el.parentNode.classList.remove('appear');
         }
       });
